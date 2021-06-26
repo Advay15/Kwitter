decd72499626ae65b5db01aeeb15472fb2217184
Advay15 <79146419+Advay15@users.noreply.github.com>
2021-06-26 14:55:58 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC_PRfrIm3KLOkzYhQVHbWvUhVHJ5-e0lw",
+    authDomain: "fifth-upgrade-295411.firebaseapp.com",
+    databaseURL: "https://fifth-upgrade-295411-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fifth-upgrade-295411",
+    storageBucket: "fifth-upgrade-295411.appspot.com",
+    messagingSenderId: "507973723078",
+    appId: "1:507973723078:web:5bfb6d12243d3f47e688c7"
+};
+firebase.initializeApp(firebaseConfig);
+
+function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+
+        });
+    });
+}
+getData();
